03d5929fa9d3a1b1f6d96ee64a6fbfb024b88702
TheMangoRango <aleppcacho@gmail.com>
2026-02-24 21:10:17 -0600

    Creación de la conexión con mysql

--- javasql.java/src/App.java
+++ javasql.java/src/App.java
@@ -1,5 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        Customer c1 = new Customer(1, "Roberto", "García", 28, "robert@email.com");
+        Customer c2 = new Customer(1, "Rodrigo", "Gonzalez", 31, "rodri@email.com");
+
+        MySqlCRUD conn = new MySqlCRUD();
     }
 }
